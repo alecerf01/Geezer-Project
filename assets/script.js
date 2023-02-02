2be3409fd3dc950bfbe1bd697d4b1c506bb74d23
Berserk <alexlecerf0131@gmail.com>
2023-02-02 18:49:44 +0000

    added bootstrap structure to html, added starter code for javascript and used jquery and ajax to call server api deezer and dynamically create html elements

---
+++ assets/script.js
@@ -0,0 +1,54 @@
+// const geniusAPI = {
+// 	"async": true,
+// 	"crossDomain": true,
+// 	"url": "https://genius-song-lyrics1.p.rapidapi.com/search/?q=enemy&per_page=10&page=1",
+// 	"method": "GET",
+// 	"headers": {
+// 		"X-RapidAPI-Key": "8053b157d6mshabf6994a388aac1p1441a0jsn89c2ee3a673d",
+// 		"X-RapidAPI-Host": "genius-song-lyrics1.p.rapidapi.com"
+// 	}
+// };
+
+// $.ajax(geniusAPI).done(function (response) {
+// 	console.log(response);
+//     console.log(response.hits[0].result.path)
+// });
+
+var songName = "enemy league"
+
+const deezerAPI = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://deezerdevs-deezer.p.rapidapi.com/search?q=" + songName,
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Key": deezerAPIKey,
+		"X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com"
+	}
+};
+
+$.ajax(deezerAPI).then(function (response) {
+	console.log(response.data[0].title)
+	console.log(response);
+
+	// song data
+
+	var title = $("<div>").addClass("song-name text-center").text("Song: " + response.data[0].title);
+	
+	var albumImage = $("<img>").addClass("album-image").attr("src", response.data[0].album.cover_big)
+	.css({"border-radius": "10px", "height":"300px"});
+	
+	var artist = $("<div>").addClass("artist text-center").text("Artist: " + response.data[0].artist.name)
+
+	// audio element
+
+	var audioEl = $("<audio>").attr("controls","")
+	
+	var songSample = $("<source>").attr({"src": response.data[0].preview, "type": "audio/mpeg"})
+	
+	
+	// Appending elements to HTML body
+	audioEl.append(songSample)
+	$("#song-container").append(title, albumImage, artist, audioEl);
+	// $("#mp3").append(audioEl)
+});
